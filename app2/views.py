2eb3e20c56aced41b94a7326e0a240639e320c06
HemaniKalaga <hemanikalaga002@gmail.com>
2023-11-08 17:15:22 +0530

    first commit

---
+++ app2/views.py
@@ -0,0 +1,6 @@
+from django.shortcuts import render
+from django.http import HttpResponse 
+
+# Create your views here.
+def func2(response):
+    return HttpResponse('<h1> <marquee> Hi This is the project2 s view fucntion in app2 </marquee>'</h1>)
